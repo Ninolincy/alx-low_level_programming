721d455c689cb904249e063be5c340af03f51bfe
Ninolincy <lincy3akoth@gmail.com>
2023-02-16 09:17:44 -0800

    5-printf.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints with puts function
+ * return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
