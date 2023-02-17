f84b811fd675dde9eb0a2a4a99380fefcf1910d9
Ninolincy <lincy3akoth@gmail.com>
2023-02-17 08:12:31 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -  A program that prints the alphabet in lowercase
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	char C;
+
+	for (C = 'a'; C <= 'z'; C++)
+		putchar(C);
+	putchar('\n');
+		return (0);
+}
