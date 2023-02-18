dc21295404f5919c6cc913d5e61c7bf7a846f64a
Ninolincy <lincy3akoth@gmail.com>
2023-02-17 17:55:24 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - A program that prints all possible combinations
+ * * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	if (i != '9')
+		putchar(' ');
+	putchar(',');
+	putchar('\n');
+	return (0);
+}
