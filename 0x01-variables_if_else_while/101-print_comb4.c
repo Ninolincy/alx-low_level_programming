4ec5b413b9d5dab7550cf330df0b23738baa3e89
Ninolincy <lincy3akoth@gmail.com>
2023-02-17 18:30:19 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ */
+
+int main(void)
+{
+	int ones, tens, hunds;
+
+	for (ones = '0'; ones <= '9'; ones++)
+	{
+		for (tens = '1'; tens <= '9'; tens++)
+		{
+			for (hunds = '2'; hunds <= '9'; hunds++)
+				if (ones < tens && hunds > tens)
+				{
+					putchar(ones);
+					putchar(tens);
+					putchar(hunds);
+					if (ones != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
