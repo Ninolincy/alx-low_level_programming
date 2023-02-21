9f06618fbfeabc3f23f90c28618a249dcd2baf75
Ninolincy <lincy3akoth@gmail.com>
2023-02-21 07:43:52 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print _alphabet - in lower case
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet_x10
+ * Return: void
+ *
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char letter;
+	int num = 0;
+
+	while (num <= 9)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+	}
+	_putchar(letter);
+	{
+	_putchar('\n');
+	num++;
+	}
+}
