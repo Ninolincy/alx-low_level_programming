d94ab3fac5ff42c7e616ef1bb18c723d9c139276
Ninolincy <lincy3akoth@gmail.com>
2023-02-27 03:03:13 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse
+ * @s: String to print
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	for (x -= 1; x >= 0; x--)
+	{
+		_putchar(s[x]);
+	}
+	_putchar('\n');
+}
