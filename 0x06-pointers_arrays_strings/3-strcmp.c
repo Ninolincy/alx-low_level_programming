fd1e243495b4660f97db0385a6f62051391901be
Ninolincy <lincy3akoth@gmail.com>
2023-03-03 04:10:34 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: first string
+ * @s2: Second string
+ * Return: 0 if s1 is equal to s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (*s1 - *s2);
+}
