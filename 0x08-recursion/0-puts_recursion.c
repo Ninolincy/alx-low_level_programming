a476454a907738081b1e1cfedc4379e7e8b15516
Ninolincy <lincy3akoth@gmail.com>
2023-03-08 02:23:56 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string, followed by a new line
+ * @s: String to be printed
+ * Return: Void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
+
