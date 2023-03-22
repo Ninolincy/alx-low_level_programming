f1cb3856ee3b2f216444a52946b10d89ce4d6a67
Ninolincy <lincy3akoth@gmail.com>
2023-03-22 07:46:06 -0700

    Calculator operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two integere
+ * @a: First integer
+ * @b: Second integer
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integere
+ * @a: First integer
+ * @b: Second integer
+ * Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two integere
+ * @a: First integer
+ * @b: Second integer
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two integere
+ * @a: First integer
+ * @b: Second integer
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculate the remainder of two integere
+ * @a: First integer
+ * @b: Second integer
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
