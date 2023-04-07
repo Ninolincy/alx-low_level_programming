b3a890c6294020b6c2cbd49354131cf17b3cef43
Ninolincy <lincy3akoth@gmail.com>
2023-04-07 08:12:05 -0700

    print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number
+ * @n: Number to print binary
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+
