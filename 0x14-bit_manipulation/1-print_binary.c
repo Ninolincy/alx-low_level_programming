cb24b7b10e3446a36bf6644a48475463e382aa13
Ninolincy <lincy3akoth@gmail.com>
2023-04-09 19:45:18 +0300

    print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,4 +9,21 @@
 
 void print_binary(unsigned long int n)
 {
+	int b;
+	int x = 0;
+	unsigned long int set;
+
+	/*bits of unsigned int is 64 in most systems*/
+	for (b = 63; b>= 0; b--)
+	{
+		set = n >> b;
+		
+		if (set & 1)
+			x++;
+		if (x)
+			_putchar((set & 1) + '0');
+	}
+	if (x == 0)
+		_putchar('0');
+}
 
