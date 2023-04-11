9ede81afe268b86bddc0c06e03e2065bb6f3c5b6
Ninolincy <lincy3akoth@gmail.com>
2023-04-11 05:32:08 -0700

    Read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 
 /**
  * read_textfile - Reads a text file and prints it to the
- * 		POSIX standard output
+ * POSIX standard output
  * @filename: Name of file to read
  * @letters: Number of letters it should read and print
  * Return: Actual number of letters it could read and print
@@ -10,7 +10,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char* buff;
+	char *buff;
 	ssize_t newFile, rd, wr;
 
 	if (filename == NULL)
