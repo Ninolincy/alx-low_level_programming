f30fa9176dd0cd3e2d06d7e3f3d717da13c1d90e
Ninolincy <lincy3akoth@gmail.com>
2023-04-11 06:20:38 -0700

    Create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,6 @@
 int create_file(const char *filename, char *text_content)
 {
 	int newFile;
-	
 	int wr;
 
 	if (filename == NULL)
@@ -21,6 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		int len = 0;
+
 		while (text_content[len])
 		{
 			len++;
