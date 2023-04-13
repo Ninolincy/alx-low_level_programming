21b7d0685230cd61af4baf0d93fc986b4135959c
Ninolincy <lincy3akoth@gmail.com>
2023-04-14 00:56:01 +0300

    Copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
+
 /**
  * main - copies the comtents of a file
  * @argc: Argument count
@@ -7,7 +11,9 @@
  */
 int main(int argc, char *argv[])
 {
-int src, dst, rd_bytes;
+int src; /*file_from*/
+int dst; /*file_to*/
+int rd_bytes, wr_bytes;
 char buff[1024];
 
 if (argc != 3)
@@ -15,27 +21,42 @@ if (argc != 3)
 dprintf(STDERR_FILENO, "Usage: %s cp file_from file_to\n", argv[0]);
 exit(97);
 }
+
 src = open(argv[1], O_RDONLY);
 if (src == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
+
 dst = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664); /*rw_rw_r__*/
-rd_bytes = read(src, buff, 1024);
-while (rd_bytes > 0)
-{
-if ((write(dst, buff, rd_bytes)) != rd_bytes || dst == -1)
+if (dst == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(src);
+exit(99);
+}
+
+rd_bytes = read(src, buff, 1024);
+
+while (rd_bytes > 0)
+{
+wr_bytes = write(dst, buff, rd_bytes);
+if (wr_bytes != rd_bytes)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 }
-if ((close(src)) == -1 || (close(dst)) == -1)
+if ((close(src)) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
 exit(100);
 }
+if ((close(dst)) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dst);
+exit(100);
+}
+
 return (0);
 }
