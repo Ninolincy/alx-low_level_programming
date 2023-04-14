8ff84da0524fb42a8f459115558e2463dae8ba53
Ninolincy <lincy3akoth@gmail.com>
2023-04-14 10:49:33 +0300

    Main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,9 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
