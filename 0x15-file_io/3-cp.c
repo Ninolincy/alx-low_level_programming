ed49253fd7cf159b43b2f83c411b727414e9bef3
Ninolincy <lincy3akoth@gmail.com>
2023-04-14 10:23:57 +0300

    Copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
-#define BUFFER_SIZE 1024
-
 /**
  * main - copies the comtents of a file
  * @argc: Argument count
@@ -11,10 +7,8 @@
  */
 int main(int argc, char *argv[])
 {
-int src; /*file_from*/
-int dst; /*file_to*/
-int rd_bytes, wr_bytes;
-char buff[1024];
+int src, dst, rd_bytes;
+char buff[BUFFER_SIZE];
 
 if (argc != 3)
 {
@@ -30,33 +24,16 @@ exit(98);
 }
 
 dst = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664); /*rw_rw_r__*/
-if (dst == -1)
+rd_bytes = read(src, buff, BUFFER_SIZE);
+if (dst == -1 || rd_bytes == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
-
-rd_bytes = read(src, buff, 1024);
-
-while (rd_bytes > 0)
-{
-wr_bytes = write(dst, buff, rd_bytes);
-if (wr_bytes != rd_bytes)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
-}
-if ((close(src)) == -1)
+if ((close(src)) == -1 || (close(dst)) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
 exit(100);
 }
-if ((close(dst)) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dst);
-exit(100);
-}
-
 return (0);
 }
