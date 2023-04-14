40b9e63e52699650384baecabd8c29b13d161409
Ninolincy <lincy3akoth@gmail.com>
2023-04-14 10:48:04 +0300

    Copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,20 +15,26 @@ if (argc != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-
 src = open(argv[1], O_RDONLY);
 if (src == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-
 dst = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664); /*rw_rw_r__*/
-rd_bytes = read(src, buff, BUFFER_SIZE);
-if (dst == -1 || rd_bytes == -1)
+
+while ((rd_bytes = read(src, buff, BUFFER_SIZE)) > 0)
+{
+if (dst == -1 || write(dst, buff, rd_bytes) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
+}
+}
+if (rd_bytes == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
 if ((close(src)) == -1 || (close(dst)) == -1)
 {
