4ff735d3f5ccaec6f9e66c4a5d967f99a46c5255
Ninolincy <lincy3akoth@gmail.com>
2023-05-04 21:16:07 +0300

    Binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,8 +12,8 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int num;
-	num = 0;
 
+	num = 0;
 	if (b == NULL)
 		return (0);
 
@@ -23,7 +23,7 @@ unsigned int binary_to_uint(const char *b)
 		{
 			num = num << 1;
 		}
-		else if (b[i] == '1') 
+		else if (b[i] == '1')
 		{
 			num = (num << 1) | 1;
 		}
