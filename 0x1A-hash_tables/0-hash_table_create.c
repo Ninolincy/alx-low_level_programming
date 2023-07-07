121fa471639313bd948072e0cd77e58d854ab03b
Ninolincy <lincyakoth@gmail.com>
2023-07-07 23:14:46 +0300

    Create hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,23 +8,24 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *hashTable;
-unsigned long int x;
-
-ht = malloc(sizeof(hash_table_t));
-if (hashTable == NULL)
-{
-return (NULL);
-}
-hashTable->size = size;
-hashTable->array = malloc(sizeof(hash_table_t *) * size);
-if (hashTable->array == NULL)
-{
-return (NULL);
-}
-for (x = 0; x < size; x++)
-{
-hashTable->array[x] = NULL;
-}
-return (hashTable);
+	hash_table_t *hashTable;
+	unsigned long int x;
+	
+	ht = malloc(sizeof(hash_table_t));
+	
+	if (hashTable == NULL)
+	{
+		return (NULL);
+	}
+	hashTable->size = size;
+	hashTable->array = malloc(sizeof(hash_table_t *) * size);
+	if (hashTable->array == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < size; x++)
+	{
+		hashTable->array[x] = NULL;
+	}
+	return (hashTable);
 }
