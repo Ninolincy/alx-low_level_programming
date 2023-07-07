3223dbc09cb13d8d85f98cc71dcbaf3599eed78d
Ninolincy <lincyakoth@gmail.com>
2023-07-07 23:16:14 +0300

    Create hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,9 +10,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashTable;
 	unsigned long int x;
-	
+
 	ht = malloc(sizeof(hash_table_t));
-	
 	if (hashTable == NULL)
 	{
 		return (NULL);
